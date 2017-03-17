8947480771c33106a3ba6735481104649cc58952
Alessandro Surace <alessandro.surace@here.com>
2017-03-17 23:20:10 +0100

    Better status display

--- Script/amazonDriveBackup.sh
+++ Script/amazonDriveBackup.sh
@@ -10,9 +10,13 @@ source=$*
 
 for i in $source
 do 
+    echo "Start process of $i"
+    date
     cleansource=$(echo $i | sed 's/\///g')
     tar jcf - ${cleansource} | gpg -q -e  > ${cleansource}.tar.bz2.gpg
-    cat $sample_img ${cleansource}.tar.bz2.gpg > ${cleansource}.tar.bz2.gpg.png
-    rclone -q copy ${cleansource}.tar.bz2.gpg.png $rclonedest:Bilder/ && rm -f ${cleansource}.tar.bz2.gpg.png ${cleansource}.tar.bz2.gpg
+    echo "Created Archive ${cleansource}.tar.bz2.gpg"
+    cat $sample_img ${cleansource}.tar.bz2.gpg > ${cleansource}.tar.bz2.gpg.png && rm ${cleansource}.tar.bz2.gpg
+    rclone -q copy ${cleansource}.tar.bz2.gpg.png $rclonedest:Bilder/ && rm -f ${cleansource}.tar.bz2.gpg.png 
+    echo "End process of $i"
 done
 
