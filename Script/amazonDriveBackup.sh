ee8ab9f31668eb67c82a5ffe25c15c002582aa72
Alessandro Surace <zioalex@gmail.com>
2017-03-21 07:12:45 +0100

    WIP split and prepend function

--- Script/amazonDriveBackup.sh
+++ Script/amazonDriveBackup.sh
@@ -8,15 +8,25 @@ sample_img=~/bin/index.png # 2035 bytes
 
 source=$*
 
+function split() {
+	split 
+}
+
+function prepend() {
+    cat $sample_img ${cleansource}.tar.bz2.gpg > ${cleansource}.tar.bz2.gpg.png && rm ${cleansource}.tar.bz2.gpg
+}
+
 for i in $source
 do 
     echo "Start process of $i"
     date
     cleansource=$(echo $i | sed 's/\///g')
     tar jcf - ${cleansource} | gpg -q -e  > ${cleansource}.tar.bz2.gpg
+    # Split gpg file
     echo "Created Archive ${cleansource}.tar.bz2.gpg"
+    # create splitted file with the prepend image
     cat $sample_img ${cleansource}.tar.bz2.gpg > ${cleansource}.tar.bz2.gpg.png && rm ${cleansource}.tar.bz2.gpg
-    rclone -q copy ${cleansource}.tar.bz2.gpg.png $rclonedest:Bilder/ && rm -f ${cleansource}.tar.bz2.gpg.png 
+    rclone --drive-chunk-size=1024k --stats=30m copy ${cleansource}.tar.bz2.gpg.png $rclonedest:Bilder/ && rm -f ${cleansource}.tar.bz2.gpg.png 
     echo "End process of $i"
 done
 
